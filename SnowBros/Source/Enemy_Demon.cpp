197a511189cb27bd952257f0b261160cc2f9fae2
himar33 <himar33@users.noreply.github.com>
2020-04-19 16:45:47 +0200

    Update Enemy_Demon.cpp

--- SnowBros/Source/Enemy_Demon.cpp
+++ SnowBros/Source/Enemy_Demon.cpp
@@ -5,14 +5,16 @@
 
 Enemy_Demon::Enemy_Demon(int x, int y) : Enemy(x, y)
 {
+	//Idle animation
 	idle.PushBack({ 3,2,29,30 });
-	currentAnim = &idle;
+
+	//Walk Animation
 
 
 	// TODO 3: Have the Brown Cookies describe a path in the screen
 
-	/*path.PushBack({ -0.4f, -0.3f }, 150, &fly);
-	path.PushBack({ -0.4f, 0.3f }, 150, &fly);*/
+	path.PushBack({ 0.3f, 0.0f }, 150, &idle);
+	//path.PushBack({ -0.4f, 0.3f }, 150, &idle);
 
 	collider = App->collisions->AddCollider({position.x, position.y, 32 * 4 - 15, 32 * 4 - 15}, Collider::Type::ENEMY, (Module*)App->enemies);
 }
@@ -20,8 +22,9 @@ Enemy_Demon::Enemy_Demon(int x, int y) : Enemy(x, y)
 void Enemy_Demon::Update()
 {
 
-	/*path.Update();
-	position = spawnPos + path.GetRelativePosition();*/
+	path.Update();
+	position = spawnPos + path.GetRelativePosition();
+	currentAnim = path.GetCurrentAnimation();
 
 	// Call to the base class. It must be called at the end
 	// It will update the collider depending on the position
