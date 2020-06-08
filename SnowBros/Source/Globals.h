73223405219d9cd11d6e5c45e2ed283a6b46e852
Denisdrk6 <denisdsuniversidad@hotmail.com>
2020-06-08 17:26:34 +0200

    Update Globals.h

--- SnowBros/Source/Globals.h
+++ SnowBros/Source/Globals.h
@@ -23,9 +23,6 @@ typedef unsigned int uint;
 #define SCREEN_WIDTH 1440
 #define SCREEN_HEIGHT 995
 
-/*#define SCREEN_WIDTH 1080
-#define SCREEN_HEIGHT 720*/
-
 #define SCREEN_SIZE 1
 #define WIN_FULLSCREEN 0
 #define WIN_FULLSCREEN_DESKTOP 1
