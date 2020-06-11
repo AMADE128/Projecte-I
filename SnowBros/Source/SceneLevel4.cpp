4e2b2ac5d32c336f4b1c87e8f99e3a4e02bb983a
Marc Pavon Llop <60131792+Marckitus@users.noreply.github.com>
2020-06-11 23:37:38 +0200

    Lv 4 walls fixed

--- SnowBros/Source/SceneLevel4.cpp
+++ SnowBros/Source/SceneLevel4.cpp
@@ -52,6 +52,8 @@ bool SceneLevel4::Start()
 	App->collisions->AddCollider({ 630, 777, 185, 2 }, Collider::Type::AIR);
 	App->collisions->AddCollider({ 630, 622, 185, 2 }, Collider::Type::AIR);
 	App->collisions->AddCollider({ 360, 460, 280, 2 }, Collider::Type::AIR);
+	App->collisions->AddCollider({ 40, 460, 280, 2 }, Collider::Type::AIR);
+	App->collisions->AddCollider({ 1190, 460, 280, 2 }, Collider::Type::AIR);
 	App->collisions->AddCollider({ 985, 316, 200, 2 }, Collider::Type::AIR);
 	App->collisions->AddCollider({ 40, 155, 400, 2 }, Collider::Type::AIR);
 	App->collisions->AddCollider({ 1250, 155, 200, 2 }, Collider::Type::AIR);
@@ -60,7 +62,9 @@ bool SceneLevel4::Start()
 	App->collisions->AddCollider({ 160, 630, 2, 150 }, Collider::Type::WALLPUSHR);
 	App->collisions->AddCollider({ 1260, 630, 2, 150 }, Collider::Type::WALLPUSHL);
 	App->collisions->AddCollider({ 220, 325, 2, 150 }, Collider::Type::WALLPUSHR);
+	App->collisions->AddCollider({ 170, 325, 2, 90 }, Collider::Type::WALLPUSHL);
 	App->collisions->AddCollider({ 1210, 165, 2, 300 }, Collider::Type::WALLPUSHL);
+	App->collisions->AddCollider({ 1260, 165, 2, 240 }, Collider::Type::WALLPUSHR);
 
 	//First two columns colliders
 	App->collisions->AddCollider({ 0, 0, 40, 1390 }, Collider::Type::WALL);
--- SnowBros/Source/SceneLevel5.cpp
+++ SnowBros/Source/SceneLevel5.cpp
@@ -32,7 +32,7 @@ bool SceneLevel5::Start()
 
 	bool ret = true;
 
-	bgTexture = App->textures->Load("Assets/Sprites/Maps/lvl1.png");
+	bgTexture = App->textures->Load("Assets/Sprites/Maps/lvl5.png");
 	App->audio->PlayMusic("Assets/Audio/Music/Stages/1.#035 Yukidama-Ondo (Stage 1, 3).ogg", 1.0f);
 
 	//Bottomside collider
